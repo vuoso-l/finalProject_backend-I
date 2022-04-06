1eb568295201101700f3b251ca6aca36f73fbd3d
vuoso-l <vuosolucas@gmail.com>
2022-04-06 11:10:11 -0300

    feat: added method to find a patient with an email

--- src/main/java/com/example/finalProject_backendI/repository/IPatientRepository.java
+++ src/main/java/com/example/finalProject_backendI/repository/IPatientRepository.java
@@ -2,8 +2,15 @@ package com.example.finalProject_backendI.repository;
 
 import com.example.finalProject_backendI.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 @Repository
 public interface IPatientRepository extends JpaRepository<Patient, Integer> {
+
+    @Query("select p from patients p where p.email like %:email%")
+    Set<Patient> findOnePatientByEmail(@Param("email") String email);
 }
