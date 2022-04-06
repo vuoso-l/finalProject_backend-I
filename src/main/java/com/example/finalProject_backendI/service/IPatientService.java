6fd702804a24289a4fb98f4f6f50463bef6e632a
vuoso-l <vuosolucas@gmail.com>
2022-04-06 11:11:02 -0300

    feat: created patient service

--- src/main/java/com/example/finalProject_backendI/service/IPatientService.java
+++ src/main/java/com/example/finalProject_backendI/service/IPatientService.java
@@ -1,2 +1,20 @@
-package com.example.finalProject_backendI.service;public interface IPatientService {
+package com.example.finalProject_backendI.service;
+
+import com.example.finalProject_backendI.DTO.PatientDTO;
+
+import java.util.Collection;
+import java.util.Set;
+
+public interface IPatientService {
+    public void createPatient(PatientDTO patientDTO);
+
+    public PatientDTO findOnePatient(Integer id);
+
+    public void updatePatient(PatientDTO patientDTO);
+
+    public void deletePatient(Integer id);
+
+    public Collection<PatientDTO> findAllPatients();
+
+    Set<PatientDTO> findOnePatientByEmail(String email);
 }
--- src/main/java/com/example/finalProject_backendI/service/PatientService.java
+++ src/main/java/com/example/finalProject_backendI/service/PatientService.java
@@ -1,2 +1,70 @@
-package com.example.finalProject_backendI.service;public class PatientService {
+package com.example.finalProject_backendI.service;
+
+import com.example.finalProject_backendI.DTO.DentistDTO;
+import com.example.finalProject_backendI.DTO.PatientDTO;
+import com.example.finalProject_backendI.entity.Dentist;
+import com.example.finalProject_backendI.entity.Patient;
+import com.example.finalProject_backendI.repository.IPatientRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.*;
+
+public class PatientService implements IPatientService{
+
+    @Autowired
+    private IPatientRepository iPatientRepository;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Override
+    public void createPatient(PatientDTO patientDTO) {
+        savePatientDTO(patientDTO);
+    }
+
+    @Override
+    public PatientDTO findOnePatient(Integer id) {
+        PatientDTO patientDTO = null;
+        Optional<Patient> patientOptional= iPatientRepository.findById(id);
+        if (patientOptional.isPresent()){
+            patientDTO = mapper.convertValue(patientOptional, PatientDTO.class);
+        }
+        return patientDTO;
+    }
+
+    @Override
+    public void updatePatient(PatientDTO patientDTO) {
+        savePatientDTO(patientDTO);
+    }
+
+    @Override
+    public void deletePatient(Integer id) {
+        iPatientRepository.deleteById(id);
+    }
+
+    @Override
+    public Collection<PatientDTO> findAllPatients() {
+        List<Patient> patientList= iPatientRepository.findAll();
+        Set<PatientDTO> patientDTOS= new HashSet<>();
+        for (Patient patient: patientList){
+            patientDTOS.add(mapper.convertValue(patient, PatientDTO.class));
+        }
+        return patientDTOS;
+    }
+
+    @Override
+    public Set<PatientDTO> findOnePatientByEmail(String email) {
+        Set<Patient> patientSet = iPatientRepository.findOnePatientByEmail(email);
+        Set<PatientDTO> patientDTOSet = new HashSet<PatientDTO>();
+        for(Patient patient: patientSet)
+            patientDTOSet.add(mapper.convertValue(patient,PatientDTO.class));
+
+        return patientDTOSet;
+    }
+
+    public void savePatientDTO (PatientDTO patientDTO){
+        Patient patient = mapper.convertValue(patientDTO, Patient.class);
+        iPatientRepository.save(patient);
+    }
 }
