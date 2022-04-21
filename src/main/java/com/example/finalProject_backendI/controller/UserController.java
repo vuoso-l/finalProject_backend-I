366200feed05c27e0efc633332c4b9aacbfe8f6a
vuoso-l <vuosolucas@gmail.com>
2022-04-21 16:34:47 -0300

    fix: deleted controllers to make posible the redirect to the index.html

--- src/main/java/com/example/finalProject_backendI/controller/UserController.java
+++ src/main/java/com/example/finalProject_backendI/controller/UserController.java
@@ -7,16 +7,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class UserController {
-    @GetMapping("/")
-    public String home(){
-        return "<h1>Bienvenido</h1>";
-    }
-    @GetMapping("/user")
-    public String user(){
-        return "<h1>Bienvenido user</h1>";
-    }
-    @GetMapping("/admin")
-    public String admin(){
-        return "<h1>Bienvenido admin</h1>";
-    }
 }
